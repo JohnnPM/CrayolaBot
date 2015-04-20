cd383bb95b54077da2fe825256664551704ce0eb
Johnn <me@Johnn.org>
2015-04-20 00:38:11 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -55,7 +55,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "Crayola Bot",
+        botName: "Crayola-Bot",
         language: "english",
         startupCap: 1, // 1-200
         startupVolume: 0, // 0-100
